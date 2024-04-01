42fb0bc266bbc2491eb50488cfb8c0918a930643
limonite <92550714+limonite@users.noreply.github.com>
2024-04-01 18:33:00 +0300

    Add files via upload

---
+++ include/Automata.h
@@ -0,0 +1,29 @@
+// Copyright 2022 GHA Test Team
+
+#pragma once
+#include <string.h>
+#include <map>
+#include <list>
+
+enum states { OFF, WAIT, ACCEPT, CHECK, COOK };
+
+class Automata
+{
+private:
+
+	float cash;
+    map<string, int> prices;
+	states state;
+public:
+    Automata();
+    void on();
+    void off();
+    void coin(int money);
+    void getMenu();
+    states getState();
+    void choice(std::string name);
+    bool check(std::string name);
+    void cancel();
+    void cook(std::string name);
+    void finish(std::string name);
+};
